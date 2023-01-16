f23513f1f46778ed5960e895feb01467c203ac22
serguney-gumus <gumus.serguney@gmail.com>
2023-01-16 15:14:03 +0300

    workflow exercises added

---
+++ queries/5_insert_workflow_exercise.sql
@@ -0,0 +1,8 @@
+INSERT INTO workflow_exercise (fk_exercise_id, fk_workflow_id, explanation, order_)
+values (1, 1, 'the first step',1);
+
+INSERT INTO workflow_exercise (fk_exercise_id, fk_workflow_id, explanation, order_)
+values (2, 1, 'the second step',2);
+
+INSERT INTO workflow_exercise (fk_exercise_id, fk_workflow_id, explanation, order_)
+values (3, 1, 'the third step',3);

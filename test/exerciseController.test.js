8ec395118c394e2cb54f1dac5278215a60c1316c
İbrahim <ibrahim@Ibrahim-Air.mshome.net>
2023-05-26 21:16:14 +0300

    response timing issue

--- test/exerciseController.test.js
+++ test/exerciseController.test.js
@@ -7,20 +7,16 @@ describe('getExercises', function () {
     it('should retrieve a list of exercises', function (done) {
         const response = {
             status: function (statusCode) {
-                // Assert that the status code is 200
                 expect(statusCode).to.eql(200);
                 return this;
             },
             json: function (data) {
-                // Assert that the response data is an array
                 expect(data).to.be.an('array');
-
-                // Assert that each item in the array is an instance of Exercise
                 data.forEach((exercise) => {
                     expect(exercise).to.be.an.instanceOf(Exercise);
                 });
 
-                done();
+                done(); // Call done() to indicate that the test is complete
             },
         };
 
--- test/workflowController.test.js
+++ test/workflowController.test.js
@@ -1,68 +1,34 @@
 /* eslint-disable node/no-unpublished-require */
 const expect = require('chai').expect;
-const sinon = require('sinon');
-const { getWorkflows } = require('../controllers/workflowController.js');
+const {
+    saveWorkflowProgress,
+} = require('../controllers/workflowController.js');
 
-describe('getWorkflows', function () {
-    it('should retrieve all workflows', function (done) {
-        // Mock the necessary dependencies
-        const response = {
-            status: sinon.spy(),
-            json: sinon.spy(),
-        };
-        const db = {
-            query: sinon.stub(),
-        };
+describe('saveWorkflowProgress', function () {
+    this.timeout(5000); // Increase timeout value to 5000ms or more if neede
 
-        // Set up the mock query function to invoke the callback with sample workflows
-        const sampleWorkflows = [
-            {
-                workflow_id: 1,
-                workflow_name: 'Workflow 1',
-                workflow_description: 'Description 1',
+    it('should save workflow progress for a user', async function () {
+        // Create mock request and response objects
+        const request = {
+            body: {
+                workflowName: 'your-workflow-name',
+                userId: 1,
+                score: 10,
             },
-            {
-                workflow_id: 2,
-                workflow_name: 'Workflow 2',
-                workflow_description: 'Description 2',
-            },
-        ];
-        db.query.callsFake(function (query, callback) {
-            callback(null, { rows: sampleWorkflows });
-        });
-
-        // Call the getWorkflows function
-        getWorkflows({}, response, db, () => {
-            // Verify the response
-            expect(response.status.calledWith(200)).to.be.true;
-            expect(response.json.calledWith(sampleWorkflows)).to.be.true;
-            done();
-        });
-    });
-
-    it('should handle database query error', function (done) {
-        // Mock the necessary dependencies
+        };
         const response = {
-            status: sinon.spy(),
-            json: sinon.spy(),
-        };
-        const db = {
-            query: sinon.stub(),
+            status: function (statusCode) {
+                // Assert that the status code is 200
+                expect(statusCode).to.eql(200);
+                return this;
+            },
+            json: function (data) {
+                // Assert that the response data contains the success message
+                expect(data).to.eql({ msg: 'success' });
+            },
         };
 
-        // Set up the mock query function to simulate a database error
-        const mockError = new Error('Database error');
-        db.query.callsFake(function (query, callback) {
-            callback(mockError);
-        });
-
-        // Call the getWorkflows function
-        getWorkflows({}, response, db, (error) => {
-            // Verify the response
-            expect(error).to.equal(mockError);
-            expect(response.status.calledWith(400)).to.be.true;
-            expect(response.json.calledWith(mockError.message)).to.be.true;
-            done();
-        });
+        await saveWorkflowProgress(request, response);
+        // Call the saveWorkflowProgress function
     });
 });

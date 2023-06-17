e8e4ee607e0b9859cfcc0c930018ae062113e3f8
İbrahim <ibrahim@Ibrahim-Air.mshome.net>
2023-06-17 14:51:07 +0300

    import wfexercise data

--- db/4_insert_workflow_exercise.sql
+++ db/4_insert_workflow_exercise.sql
@@ -1,14 +1,30 @@
-INSERT INTO workflow_exercise (fk_exercise_id, fk_workflow_id, explanation, order_)
-values (1, 1, 'the first step',1);
 
 INSERT INTO workflow_exercise (fk_exercise_id, fk_workflow_id, explanation, order_)
-values (2, 1, 'the second step',2);
+values (1, 1, '',1);
+INSERT INTO workflow_exercise (fk_exercise_id, fk_workflow_id, explanation, order_)
+values (2, 1, '',2);
+INSERT INTO workflow_exercise (fk_exercise_id, fk_workflow_id, explanation, order_)
+values (3, 1, '',3);
+INSERT INTO workflow_exercise (fk_exercise_id, fk_workflow_id, explanation, order_)
+values (4, 1, '',4);
+INSERT INTO workflow_exercise (fk_exercise_id, fk_workflow_id, explanation, order_)
+values (5, 1, '',5);
+INSERT INTO workflow_exercise (fk_exercise_id, fk_workflow_id, explanation, order_)
+values (6, 1, '',6);
+INSERT INTO workflow_exercise (fk_exercise_id, fk_workflow_id, explanation, order_)
+values (7, 1, '',7);
+INSERT INTO workflow_exercise (fk_exercise_id, fk_workflow_id, explanation, order_)
+values (8, 1, '',8);
+INSERT INTO workflow_exercise (fk_exercise_id, fk_workflow_id, explanation, order_)
+values (9, 1, '',9);
+INSERT INTO workflow_exercise (fk_exercise_id, fk_workflow_id, explanation, order_)
+values (10, 1, '',10);
+INSERT INTO workflow_exercise (fk_exercise_id, fk_workflow_id, explanation, order_)
+values (11, 1, '',11);
+
 
 INSERT INTO workflow_exercise (fk_exercise_id, fk_workflow_id, explanation, order_)
-values (3, 1, 'the third step',3);
+values (1, 2, '',1);
 
 INSERT INTO workflow_exercise (fk_exercise_id, fk_workflow_id, explanation, order_)
-values (1, 2, 'the first step',1);
-
-INSERT INTO workflow_exercise (fk_exercise_id, fk_workflow_id, explanation, order_)
-values (2, 3, 'the first step',1);
+values (2, 3, '',1);
